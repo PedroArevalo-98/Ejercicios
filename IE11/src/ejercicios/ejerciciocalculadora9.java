c6f8635df728ad143fbf8bd956474a3ab7f25f18
usuario <usuario@usuario-GB-BPCE-3455>
2019-10-15 14:23:46 +0200

    ejercicio9

---
+++ IE11/src/ejercicios/ejerciciocalculadora9.java
@@ -0,0 +1,30 @@
+package ejercicios;
+
+import java.security.DrbgParameters.NextBytes;
+import java.util.Scanner;
+
+public class ejerciciocalculadora9 {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		Scanner entrada = new Scanner(System.in);
+		int instruccion = 0;
+		System.out.println("Introducir una instruccion para la calculadora");
+		Scanner 
+		System.out.println("1 Sumar numeros");
+		System.out.println("2 Restar numeros");
+		System.out.println("3 Multiplicar los numeros");
+		System.out.println("4 Dividir loa numeros");
+		System.out.println("5 salir del programa");
+		
+	while (instruccion != 5) {
+		instruccion= entrada.nextInt()
+				if (instruccion == 1) {
+					
+				}
+	}
+			
+		}
+	}
+	}
